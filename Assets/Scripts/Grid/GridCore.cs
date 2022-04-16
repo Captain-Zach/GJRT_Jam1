72a46509426e86b6c445536a6d08bc77bc5cbd6b
Zachary Jones <zacharyjones@microsoft.com>
2022-04-16 15:28:55 -0700

    Now keeping a reference of every tile on the grid.

--- Assets/Scripts/Grid/GridCore.cs
+++ Assets/Scripts/Grid/GridCore.cs
@@ -8,13 +8,18 @@ public class GridCore : MonoBehaviour
     [SerializeField] int Width;
     [SerializeField] int Height;
 
-    [SerializeField] float gridSpacing;
     [SerializeField] int refreshDelay;
 
     [SerializeField] GameObject PlaceHolder;
 
-    // Need private nested lists to hold the changing size of the grid.
-   public List<List<GameObject>> gridMap;
+    private float gridSpacing = 1;
+    // Toggle these for testing
+    // public float gridSpacing = 20;
+
+   public List<List<GameObject>> gridMap = new List<List<GameObject>>() {};
+
+   // Instead of listing lists of game objects, why don't we... Create a tile class
+   // and keep a nested list of that? We could put anything in it that inherits from it. 
 
     /* 
         Imagine something like this when given a map with a size of 4 by 4:
@@ -28,26 +33,31 @@ public class GridCore : MonoBehaviour
 
     void Start()
     {
+        // Going to loop through
         for(int y = 0; y< Height; y++)
         {
+            // This is a bit like building the train tracks as the train is running over it.
+            gridMap.Add(new List<GameObject>(){});
+            
             for(int x = 0; x < Width; x++)
             {
                 // Instantiate
                 Vector2 TestBoi = new Vector2(x * gridSpacing, y * gridSpacing);
-                GameObject Tile = Instantiate(PlaceHolder, TestBoi, this.transform.rotation);
-
-                // assign each sprite to it's place in the grid.
-                // gridMap[y][x] = new GameObject();
-                // gridMap[y][x] = Tile;
+                GameObject Tile = Instantiate(PlaceHolder, this.transform);
+                gridMap[y].Add(Tile);
+                
             }
         }
+
+        Debug.Log(gridMap);
+        Debug.Log(gridMap[0][0]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // dynaSpacing();
-        // Debug.Log(gridMap);
+        dynaSpacing();
+        
     }
 
     void dynaSpacing()
