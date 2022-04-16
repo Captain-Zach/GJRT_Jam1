e932edfb2cb36463c1f6e1d89b22abf93f1187b6
Zachary Jones <zacharyjones@microsoft.com>
2022-04-16 11:13:33 -0700

    Found spacing sweet spot at 1, changing spacing to private.

--- Assets/Scripts/Grid/GridCore.cs
+++ Assets/Scripts/Grid/GridCore.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridCore : MonoBehaviour
@@ -13,7 +14,7 @@ public class GridCore : MonoBehaviour
     [SerializeField] GameObject PlaceHolder;
 
     // Need private nested lists to hold the changing size of the grid.
-    private List<List<GameObject>> gridMap;
+   public List<List<GameObject>> gridMap;
 
     /* 
         Imagine something like this when given a map with a size of 4 by 4:
@@ -33,8 +34,11 @@ public class GridCore : MonoBehaviour
             {
                 // Instantiate
                 Vector2 TestBoi = new Vector2(x * gridSpacing, y * gridSpacing);
-                Instantiate(PlaceHolder, TestBoi, this.transform.rotation);
+                GameObject Tile = Instantiate(PlaceHolder, TestBoi, this.transform.rotation);
+
                 // assign each sprite to it's place in the grid.
+                // gridMap[y][x] = new GameObject();
+                // gridMap[y][x] = Tile;
             }
         }
     }
@@ -42,7 +46,25 @@ public class GridCore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // dynaSpacing();
+        // Debug.Log(gridMap);
+    }
+
+    void dynaSpacing()
+    {
+        int xTracker = 0;
+        int yTracker = 0;
+        foreach(List<GameObject> row in gridMap)
+        {
+            foreach (GameObject Tile in row)
+            {
+                // Tile.transform.position = new Vector2(xTracker * gridSpacing, yTracker * gridSpacing);
+                xTracker++;
+                Debug.Log(xTracker + " " + yTracker);
+            }
+            xTracker = 0;
+            yTracker++;
+        }
     }
 
 }
